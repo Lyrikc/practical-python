a607e385cc63018047086d7f8a04f9cfd9602f02
Daria <daria.shishkina@waveaccess.global>
2024-03-11 17:30:56 +0400

    Updated pcost.py file for exercise 1.31

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,13 +3,32 @@
 # Exercise 1.27
 def portfolio_cost(filename):
 	total_cost = 0.0
+	skip = False
 
 	with open(filename,'rt') as f:
 		next(f)
+
 		for line in f:
 			row = line.split(',')
-			row[2] = row[2][0:len(row[2])-1]
-			total_cost = total_cost+ int(row[1])* float(row[2])
+			try:
+				row[1] = int(row[1])
+			except ValueError:
+				print("Couldn't parse amount of the shares from here:", line)
+				skip = True
+			try:
+				row[2] = float(row[2][0:len(row[2])-1])
+			except ValueError:
+				print("Couldn't parse price of the shares from here:", line)
+				skip = True
+
+			if skip == True:
+				skip = False
+			else:
+				total_cost = total_cost + row[1] * row[2]
+
+
+
+
 	return total_cost;
 
 	
