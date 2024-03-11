1acd9a55f66e3fe7fca1200b394bdb08105263ff
Daria <daria.shishkina@waveaccess.global>
2024-03-11 16:23:02 +0400

    Updated pcost.py file for exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
-total_cost = 0.0
+def portfolio_cost(filename):
+	total_cost = 0.0
 
-with open('Data/portfolio.csv','rt') as f:
-	next(f)
-	for line in f:
-		row = line.split(',')
-		row[2] = row[2][0:len(row[2])-1]
-		total_cost = total_cost+ int(row[1])* float(row[2])
+	with open(filename,'rt') as f:
+		next(f)
+		for line in f:
+			row = line.split(',')
+			row[2] = row[2][0:len(row[2])-1]
+			total_cost = total_cost+ int(row[1])* float(row[2])
+	return total_cost;
 
-print(f'Total cost is {total_cost:0.2f}')		
+	
 
+#cost = portfolio_cost('Data/portfolio.csv');
+
+#print(f'Total cost is {cost:0.2f}')	
