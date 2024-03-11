a9d639aacc1f2fac356265a1e01b58c47e880621
Daria <daria.shishkina@waveaccess.global>
2024-03-11 15:51:23 +0400

    Adding pcost.py file for exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+total_cost = 0.0
+
+with open('Data/portfolio.csv','rt') as f:
+	next(f)
+	for line in f:
+		row = line.split(',')
+		row[2] = row[2][0:len(row[2])-1]
+		total_cost = total_cost+ int(row[1])* float(row[2])
+
+print(f'Total cost is {total_cost:0.2f}')		
+
